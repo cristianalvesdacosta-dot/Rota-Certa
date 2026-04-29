2d11aa1e916980d4ade24a0c84880c24d6b02cd6
cristianalvesdacosta-dot <cristianalvesdacosta@gmail.com>
2026-04-28 22:09:21 -0300

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCX1k0PN_yOd3QWauBQhwX0FRwqhn7Clo",
+    authDomain: "gestao-financeira-motoboy.firebaseapp.com",
+    databaseURL: "https://gestao-financeira-motoboy-default-rtdb.firebaseio.com",
+    projectId: "gestao-financeira-motoboy",
+    storageBucket: "gestao-financeira-motoboy.firebasestorage.app",
+    messagingSenderId: "107405414927",
+    appId: "1:107405414927:web:71eae0a1917ce9e512897a"
+};
